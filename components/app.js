5f9ba0a6453db752340e2ab1f72df04b26c89125
vergilius <michal.zurek@arteverest.pl>
2015-12-30 21:52:22 +0100

    Setup webpack with react + jsx

--- components/app.js
+++ components/app.js
@@ -2,10 +2,36 @@ import todos from './todo/todos';
 import visibilityFilter from './filter/filter';
 
 import { createStore, combineReducers } from 'Redux';
+import React from 'React';
+import ReactDOM from 'react-dom';
 
 const todoApp = combineReducers({
   todos,
   visibilityFilter
 });
 
+const store = createStore(todoApp);
+
+class TodoApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>
+          test
+        </button>
+      </div>
+    );
+
+  }
+}
+
+const render = () => {
+  ReactDOM.render(
+    <TodoApp />,
+    document.getElementById('root')
+  );
+};
+
+store.subscribe(render);
+render();
 console.log('DONE');
